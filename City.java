ffd6cfa7adcf2abdb08a902ce90d65633d9a5acf
larrygoldman12 <71949042+larrygoldman12@users.noreply.github.com>
2020-10-17 16:21:40 -0500

    Create City.java

---
+++ City.java
@@ -0,0 +1,69 @@
+public class City implements Comparable<City> {
+
+	private String name; // name of the city
+	private int distance; // distance from Madison
+	
+	public City (String name, int dist) {
+		this.name = name;
+		this.distance = dist;
+	}
+	
+	
+	/**
+	 * Returns the distance between this city and Madison
+	 * @return the distance between this city and Madison
+	 */
+	public int getDistance() {
+		return this.distance;
+	}
+	
+	/**
+	 * Returns the name of this city
+	 * @return the name of this city
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	/**
+	 * Sets a new name for this city
+	 * @param newName - the new name for this city
+	 */
+	public void setName(String newName) {
+		this.name = newName;
+	}
+	
+	/**
+	 * Set a new distance between this city and Madison
+	 * @param dist - the new distance between this city and Madison
+	 */
+	public void setDistance(int dist) {
+		this.distance = dist;
+	}
+	
+	/**
+	 * Compares the distance of this city from Madison to the distance
+	 * from Madison of another city.
+	 * @param other - the city being compared to
+	 * @return 0 if the distances are equal, a negative number if this
+	 * 		   city is closer to Madison than the other, or a positive
+	 * 		   number if this city is further from Madison.
+	 */
+	@Override
+	public int compareTo(City other) {
+		if (this.distance == other.getDistance()) {
+			// if the distances are equal
+			return 0;
+		}
+		else if (this.distance < other.getDistance()) {
+			// if this city is closer to Madison than "other"
+			return -1;
+		}
+		else {
+			// if this city is further from Madison than "other"
+			return 1;
+		}
+	}
+
+	
+}
